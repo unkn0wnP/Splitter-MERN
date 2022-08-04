c05410089e8aca08b3b75c839411c9f2c5376ab5
unkn0wnP <princegadhiya51@gmail.com>
2022-08-04 22:58:22 +0530

    profile updated

--- frontend/src/components/Dashboard/FriendDetail.js
+++ frontend/src/components/Dashboard/FriendDetail.js
@@ -42,6 +42,7 @@ export default function FriendDetail() {
   };
 
   const handleSubmitExp = () => {
+    console.log(fname.friendID)
     seteformvalue({...eformvalue,[fname]:fname.friendID})
     addExp(username, eformvalue);
   };
