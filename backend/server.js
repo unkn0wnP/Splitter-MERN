694c84bd5c89e545fa924880239d4bda3ca5ce91
unkn0wnP <princegadhiya51@gmail.com>
2022-12-12 21:47:02 +0530

    backend package.json && server.js changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use("/",Router)
 
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 app.get("*", (request, response) => {
-  response.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
